807cd87519723bad3fa20e283d7547cbb672f4a1
Zhenis Pir <yenispiro@gmail.com>
2024-08-07 11:14:39 +0500

    installed mdsvex and modified svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import {sveltePreprocess} from 'svelte-preprocess';
+import {mdsvex} from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +11,15 @@ const config = {
 		adapter: adapter()
 	},
 
-	preprocess: [sveltePreprocess()]
+	extensions: ['.svelte', '.md'],
+
+	preprocess: [
+		sveltePreprocess(),
+		mdsvex(
+			{
+				extension: ['.md']
+			})
+	]
 };
 
 export default config;
